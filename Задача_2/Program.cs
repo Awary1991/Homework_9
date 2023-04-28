746b1e037b523256ac7ab2a9fbb9b1525343e2e8
Alexandr Odarchenko <Nolofinve1991@gmail.com>
2023-04-28 18:51:58 +0300

    Домашняя работа 9 семинар

---
+++ Задача_2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Введите начало диапазона:");
+bool isParsedStart = int.TryParse(Console.ReadLine(), out int start);
+
+Console.WriteLine("Введите конец диапазона:");
+bool isParsedStop = int.TryParse(Console.ReadLine(), out int stop);
+
+if (!isParsedStart || !isParsedStop) { Console.WriteLine("Введено не число"); return;}
+
+Console.WriteLine(SummElement(start, stop));
+
+int SummElement (int m, int n)
+{
+    
+    if (n < m) { return 0; }
+
+    n--;
+
+    return SummElement(m, n) + n+1;
+}
